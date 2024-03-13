6b5e75b6ee80c9b486ae585386081c49415c2ce0
yungreggy <alexandre.gregoire.me@protonmail.com>
2024-03-13 11:22:58 -0400

    ajout du composant Commentaires

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -3,10 +3,18 @@ import './404.css';
 
 function Page404() {
   return (
-    <div>
+    <main>
+
+      <section >
+ 
+      <img src={`${process.env.PUBLIC_URL}/img/svg/lignes-accueil.svg`} alt="lignes" />
+ <div>
       <h1>404</h1>
       <p>Page non trouvée</p>
-    </div>
+      </div>
+
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Commentaires/Commentaires.js
@@ -0,0 +1,22 @@
+import './Commentaires.css';
+import React from 'react';
+
+
+
+function Commentaires() {
+
+ 
+
+
+
+
+
+
+
+
+}
+
+
+
+
+export default Commentaires;
